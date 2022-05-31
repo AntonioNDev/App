95a9e23dcd91ce1ea04b53c4188bd1cf1ec35b4d
AntonioNDev <antonionikoloski2003@gmail.com>
2022-05-31 02:57:26 +0200

    bug fixed

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-response = requests.get("https://api.weatherapi.com/v1/forecast.json?key=d544248570e94c51a67211516211806&q=USA&days=2&aqi=yes&alerts=yes").json()
+response = requests.get("https://api.weatherapi.com/v1/forecast.json?key=d544248570e94c51a67211516211806&q=London&days=2&aqi=yes&alerts=yes").json()
 
 with open("data.json", "w+") as data:
    json.dump(response, data, indent=4)
