c5c737b8179a3502aa54b29814136c95ea5041b8
AntonioNDev <antonionikoloski2003@gmail.com>
2022-06-02 17:01:38 +0200

    V1

--- App/settings.py
+++ App/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*c=7fl*cls-5=*^%f_lx+t82nmz0ns+se#^nep+60#n_)%wp_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.107'] #192.168.1.107
+ALLOWED_HOSTS = []
 
 
 # Application definition
